46e278057741ef31449cb64ae5ff704c9bb8694b
Gospel Chukwu <brondchux@gmail.com>
2022-01-10 20:24:34 -0500

    Started working on hompage contents

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import { Segment, Grid } from "semantic-ui-react";
+import Logo from "../Logo";
+import Navbar from "../Navbar";
+const Header = () => {
+	return (
+		<Segment>
+			<Grid container stackable columns={2}>
+				<Grid.Column>
+					<Logo></Logo>
+				</Grid.Column>
+				<Grid.Column>
+					<Navbar></Navbar>
+				</Grid.Column>
+			</Grid>
+		</Segment>
+	);
+};
+export default Header;
