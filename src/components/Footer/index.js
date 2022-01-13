77475de6ccf51863cee1f967f41809c622920d06
Gospel Chukwu <brondchux@gmail.com>
2022-01-13 16:26:47 -0500

    Displayed developer details on footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import { Container, Divider } from "semantic-ui-react";
+const Footer = () => {
+	return (
+		<Container textAlign="center">
+			<Divider />
+			<p>Progressive developments by Gospel n Friends!</p>
+			<Divider />
+		</Container>
+	);
+};
+export default Footer;
