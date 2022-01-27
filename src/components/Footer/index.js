be0157dbcfb7ee38fd09900cfd30fee5d0901751
Gospel Chukwu <brondchux@gmail.com>
2022-01-26 22:28:23 -0500

    Added current year to display on footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,13 @@
+import moment from "moment";
 import { Container, Divider } from "semantic-ui-react";
 const Footer = () => {
 	return (
 		<Container textAlign="center">
 			<Divider />
-			<p>Progressive developments by Gospel n Friends!</p>
+			<p>
+				Progressive developments by Gospel n Friends! &copy; Copyright{" "}
+				{moment(Date.now()).format("YYYY")}
+			</p>
 			<Divider />
 		</Container>
 	);
